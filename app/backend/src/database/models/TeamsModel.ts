726f636edda5c8dde209bc401dee99cf2d3f44fa
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-05-22 13:48:30 -0300

    corrigindo erros de lint

--- app/backend/src/database/models/TeamsModel.ts
+++ app/backend/src/database/models/TeamsModel.ts
@@ -1,26 +1,26 @@
 import { DataTypes, Model } from 'sequelize';
 import sequelize from '.';
 
-class Teams extends Model{
-    declare id: number;
-    declare teamName: string;
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
 }
 
 Teams.init({
-     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    teamName: {
-        type: DataTypes.STRING,
-    }
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+  },
 }, {
-    tableName: 'teams',
-    sequelize,
-    timestamps: false,
-    underscored: true
-})
+  tableName: 'teams',
+  sequelize,
+  timestamps: false,
+  underscored: true,
+});
 
-export default Teams
+export default Teams;
