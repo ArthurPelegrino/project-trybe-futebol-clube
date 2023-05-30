5760673049a15ffbb7d775bdbc7189d7c2ada974
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-05-30 17:55:41 -0300

    Requisito 16

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -2,7 +2,18 @@ import { Response, Request } from 'express';
 import MatchesService from '../services/MatchService';
 
 class MatchController {
-  static async getAll(_req: Request, res: Response): Promise<Response> {
+  static async getAll(req: Request, res: Response): Promise<Response> {
+    console.log(req.query);
+    const search = req.query;
+    console.log(typeof search);
+    if (Object.values(search).includes('true')) {
+      const matchesByQueryTrue = await MatchesService.findAllByQueryTrue();
+      return res.status(200).json(matchesByQueryTrue);
+    }
+    if (Object.values(search).includes('false')) {
+      const matchesByQueryFalse = await MatchesService.findAllByQueryFalse();
+      return res.status(200).json(matchesByQueryFalse);
+    }
     const allMatches = await MatchesService.findAll();
     return res.status(200).json(allMatches);
   }
--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -3,24 +3,53 @@ import Matches from '../database/models/MatchesModel';
 
 class MatchesService {
   static async findAll(): Promise<TeamsAttributes[] | undefined> {
-    try {
-      const matches = await Matches.findAll({
-        include: [{
-          model: Teams,
-          as: 'homeTeam',
-          attributes: ['teamName'],
-        },
-        {
-          model: Teams,
-          as: 'awayTeam',
-          attributes: ['teamName'],
-        }],
-      });
+    const matches = await Matches.findAll({
+      include: [{
+        model: Teams,
+        as: 'homeTeam',
+        attributes: ['teamName'],
+      },
+      {
+        model: Teams,
+        as: 'awayTeam',
+        attributes: ['teamName'],
+      }],
+    });
+    return matches;
+  }
 
-      return matches;
-    } catch (error) {
-      console.log('erro');
-    }
+  static async findAllByQueryTrue(): Promise<TeamsAttributes[] | void> {
+    const matches = await Matches.findAll({
+      include: [{ model: Teams,
+        as: 'homeTeam',
+        attributes: ['teamName'] },
+      {
+        model: Teams,
+        as: 'awayTeam',
+        attributes: ['teamName'],
+      }],
+      where: {
+        inProgress: true,
+      },
+    });
+    return matches;
+  }
+
+  static async findAllByQueryFalse(): Promise<TeamsAttributes[] | void> {
+    const matches = await Matches.findAll({
+      include: [{ model: Teams,
+        as: 'homeTeam',
+        attributes: ['teamName'] },
+      {
+        model: Teams,
+        as: 'awayTeam',
+        attributes: ['teamName'],
+      }],
+      where: {
+        inProgress: false,
+      },
+    });
+    return matches;
   }
 }
 
